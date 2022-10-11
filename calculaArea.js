6738bdf8b4e2f03209c0032396cae8e39ef72949
Thiago <thimachal@gmail.com>
2022-10-10 22:07:32 -0300

    feito

--- calculaArea.js
+++ calculaArea.js
@@ -1,9 +1,9 @@
+let base = prompt("Digite o valor da base");
+let altura = prompt("Digite o valor da da altura");
 
 function calc(base, altura) {
     let result = 0;
-    result=base*altura
-    return result;
+    result=base*altura;
+    console.log(`O resultado é: ${result}`);
 }
-
-
 calc (base,altura);
